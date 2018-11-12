3a9eb199a72633aec24f07c7610ec05e78d65f77
Haoban <37427359+Haoban@users.noreply.github.com>
2018-11-12 23:10:24 +0200

    User Interface & Translator

--- TobiiTest/MainWindow.xaml.cs
+++ TobiiTest/MainWindow.xaml.cs
@@ -14,12 +14,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Tobii.Interaction;
+using System.Windows.Forms.Integration;
+using System.Runtime.InteropServices;
 
 namespace TobiiTest
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
 
@@ -32,18 +32,18 @@ namespace TobiiTest
             _timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
             _timer.Enabled = true;
             //OCRUtil.RecognizeImage("n48.jpg");
-            OCRUtil.RecognizeImage("Screen24229070,0274542.png");
+            OCRUtil.RecognizeImage("Screen718973825.639451.png");
         }
-
+        
         private void OnTimedEvent(object source, ElapsedEventArgs e)
         {
             //var gazePointDataStream = ((App)Application.Current).Host.Streams.CreateGazePointDataStream();
             //var gazePointDataStream = ((App)Application.Current).Host.Streams.CreateGazePointDataStream(Tobii.Interaction.Framework.GazePointDataMode.Unfiltered);
-            /*
+           
             var fixationDataStream = ((App)Application.Current).Host.Streams.CreateFixationDataStream();
             fixationDataStream.Begin((x, y, timestamp) => Dispatcher.BeginInvoke(new Action(() => {
                 lbl1.Content = "Begin fixation at X: " + x + " Y: " + y;
-                //var abc = ScreenshotUtil.Abc(x, y, timestamp);
+                //var abc = ScreenshotUtil.TakeScreen(x, y, timestamp);
                 //OCRUtil.RecognizeImage(abc);
             })));
             fixationDataStream.Data((x, y, timestamp) => Dispatcher.BeginInvoke(new Action(() =>
@@ -52,8 +52,17 @@ namespace TobiiTest
             fixationDataStream.End((x, y, timestamp) => Dispatcher.BeginInvoke(new Action(() =>
                 lbl3.Content = String.Format("End fixation at X: {0} Y: {1}", x, y)
             )));
-            */
+            
         }
 
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
