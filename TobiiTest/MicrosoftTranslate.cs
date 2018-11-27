1f3c329cca37f7ad1d868f56d7a0d1e356a2835f
Dovzhikov Sergey <illidanboss@gmail.com>
2018-11-27 22:39:59 +0200

    changed Translate method in MSTranslator to synchronous

--- TobiiTest/MicrosoftTranslate.cs
+++ TobiiTest/MicrosoftTranslate.cs
@@ -20,7 +20,9 @@ namespace TobiiTest
 
         static string text = "Hello world!";
 
-        async static string Translate(string text)
+        // async version header saved, just in case
+        // async static string Translate(string text)
+        static string Translate(string text)
         {
             System.Object[] body = new System.Object[] { new { Text = text } };
             var requestBody = JsonConvert.SerializeObject(body);
@@ -33,12 +35,13 @@ namespace TobiiTest
                 request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
                 request.Headers.Add("Ocp-Apim-Subscription-Key", key);
 
-                var response = await client.SendAsync(request);
-                var responseBody = await response.Content.ReadAsStringAsync();
+                var response = client.SendAsync(request).Result;
+                var responseBody = response.Content.ReadAsStringAsync().Result;
                 var result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody), Formatting.Indented);
 
-                Console.OutputEncoding = UnicodeEncoding.UTF8;
-                Console.WriteLine(result);
+                //Console.OutputEncoding = UnicodeEncoding.UTF8;
+                //Console.WriteLine(result);
+                return result;
             }
         }
 
