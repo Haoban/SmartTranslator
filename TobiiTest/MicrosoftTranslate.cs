b9a712d500b47ad809040d39008b5723be33a3b4
Dovzhikov Sergey <illidanboss@gmail.com>
2018-11-27 23:31:01 +0200

    added language list, made output language variable

--- TobiiTest/MicrosoftTranslate.cs
+++ TobiiTest/MicrosoftTranslate.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 // NOTE: Install the Newtonsoft.Json NuGet package.
@@ -8,17 +9,11 @@ namespace TobiiTest
 {
     class MicrosoftTranslator
     {
-        static string host = "https://api.cognitive.microsofttranslator.com";
-        static string path = "/translate?api-version=3.0";
-        // Translate to German and Italian.
-        static string params_ = "&to=de&to=it";
-
-        static string uri = host + path + params_;
-
-        // NOTE: Replace this example key with a valid subscription key.
-        static string key = "0dcb155e46094a3a8be0ba81710e4f06";
-
-        static string text = "Hello world!";
+        private static string host = "https://api.cognitive.microsofttranslator.com";
+        private static string path = "/translate?api-version=3.0";
+        private string uri;
+        
+        private static string key = "0dcb155e46094a3a8be0ba81710e4f06";
 
         public string SourceLanguage
         {
@@ -32,15 +27,96 @@ namespace TobiiTest
             set;
         }
 
+        public static Dictionary<string,string> Languages
+        {
+            get
+            {
+                if (_languages == null || _languages.Count == 0)
+                {
+                    _languages = new Dictionary<string, string>
+                    {
+                        { "Afrikaans", "af" },
+                        { "Arabic", "ar" },
+                        { "Arabic, Levantine", "apc" },
+                        { "Bangla", "bn" },
+                        { "Bosnian (Latin)", "bs" },
+                        { "Bulgarian", "bg" },
+                        { "Cantonese (Traditional)", "yue" },
+                        { "Catalan", "ca" },
+                        { "Chinese Simplified", "zh-Hans" },
+                        { "Chinese Traditional", "zh-Hant" },
+                        { "Croatian", "hr" },
+                        { "Czech", "cs" },
+                        { "Danish", "da" },
+                        { "Dutch", "nl" },
+                        { "English", "en" },
+                        { "Estonian", "et" },
+                        { "Fijian", "fj" },
+                        { "Filipino", "fil" },
+                        { "Finnish", "fi" },
+                        { "French", "fr" },
+                        { "German", "de" },
+                        { "Greek", "el" },
+                        { "Haitian Creole", "ht" },
+                        { "Hebrew", "he" },
+                        { "Hindi", "hi" },
+                        { "Hmong Daw", "mww" },
+                        { "Hungarian", "hu" },
+                        { "Icelandic", "is" },
+                        { "Indonesian", "id" },
+                        { "Italian", "it" },
+                        { "Japanese", "ja" },
+                        { "Kiswahili", "sw" },
+                        { "Klingon", "tlh" },
+                        { "Klingon (plqaD)", "tlh-Qaak" },
+                        { "Korean", "ko" },
+                        { "Latvian", "lv" },
+                        { "Lithuanian", "lt" },
+                        { "Malagasy", "mg" },
+                        { "Malay", "ms" },
+                        { "Maltese", "mt" },
+                        { "Norwegian", "nb" },
+                        { "Persian", "fa" },
+                        { "Polish", "pl" },
+                        { "Portuguese", "pt" },
+                        { "Queretaro Otomi", "otq" },
+                        { "Romanian", "ro" },
+                        { "Russian", "ru" },
+                        { "Samoan", "sm" },
+                        { "Serbian (Cyrillic)", "sr-Cyrl" },
+                        { "Serbian (Latin)", "sr-Latn" },
+                        { "Slovak", "sk" },
+                        { "Slovenian", "sl" },
+                        { "Spanish", "es" },
+                        { "Swedish", "sv" },
+                        { "Tahitian", "ty" },
+                        { "Tamil", "ta" },
+                        { "Telugu", "te" },
+                        { "Thai", "th" },
+                        { "Tongan", "to" },
+                        { "Turkish", "tr" },
+                        { "Ukrainian", "uk" },
+                        { "Urdu", "ur" },
+                        { "Vietnamese", "vi" },
+                        { "Welsh", "cy" },
+                        { "Yucatec Maya", "yua" }
+                    };
+                }
+                return _languages;
+            }
+        }
+
         public MicrosoftTranslator(string srclang, string targlang)
         {
             SourceLanguage = srclang;
             TargetLanguage = targlang;
+            Languages.TryGetValue(SourceLanguage, out string to);
+            uri = host + path + "&to=" + to;
         }
 
         // async version header saved, just in case
         // async static string Translate(string text)
-        static string Translate(string text)
+        string Translate(string text)
         {
             System.Object[] body = new System.Object[] { new { Text = text } };
             var requestBody = JsonConvert.SerializeObject(body);
@@ -70,5 +146,7 @@ namespace TobiiTest
             Console.ReadLine();
         }
         */
+
+        private static Dictionary<string, string> _languages;
     }
 }
