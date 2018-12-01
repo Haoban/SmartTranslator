ec078db9430e2188da88c3aa17cd847125d76c78
Dovzhikov Sergey <illidanboss@gmail.com>
2018-12-01 19:37:52 +0200

    added missing files to git

---
+++ TobiiTest/Translator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TobiiTest
+{
+    public abstract class Translator
+    {
+        public string SourceLanguage
+        {
+            get;
+            set;
+        }
+
+        public string TargetLanguage
+        {
+            get;
+            set;
+        }
+
+        public abstract string Translate(string text);
+
+        public static Translator Create(string translator, string src, string targ)
+        {
+            switch (translator)
+            {
+                case "Google":
+                    return new GoogleTranslator(src, targ);
+                case "Microsoft":
+                    return new MicrosoftTranslator(src, targ);
+                default:
+                    throw new ArgumentException("unknown translator: " + translator);
+            }
+        }
+    }
+}
