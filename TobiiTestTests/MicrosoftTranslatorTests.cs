7c72ca64d5e4a3f9cee89553836681aa6e3f2fe4
Dovzhikov Sergey <illidanboss@gmail.com>
2018-12-05 12:57:34 +0200

    added missing files

---
+++ TobiiTestTests/MicrosoftTranslatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TobiiTest;
+
+namespace TobiiTest.Tests
+{
+    [TestFixture()]
+    public class MicrosoftTranslatorTests
+    {
+        [Test()]
+        public void TranslateText()
+        {
+            var s = "Hello, world!";
+            var ms = new MicrosoftTranslator("English", "Finnish");
+            var tl = ms.Translate(s);
+            Assert.AreEqual("Hei, maailma!", tl);
+        }
+    }
+}
